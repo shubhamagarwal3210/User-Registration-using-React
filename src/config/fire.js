c75d03fe4a193f2425598cc13383610a725fb284
Shubham <shubhamagarwal3210@gmail.com>
2020-05-13 22:26:12 +0530

    Add Source Files

---
+++ src/config/fire.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+var config = {
+  apiKey: "###################################",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "sender-id",
+  appId: "app-id",
+  measurementId: "G-measurement-id",
+};
+
+const fire = firebase.initializeApp(config);
+export default fire;
